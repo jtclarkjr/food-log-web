fb19621577d4d8171a96176ec431dd12d4c7a12c
James <jtclarkjr@ucla.edu>
2025-01-20 21:04:39 +0900

    Update unauthorized.tsx

--- app/unauthorized.tsx
+++ app/unauthorized.tsx
@@ -1,40 +1,5 @@
-'use client'
-
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
-import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
 export default function Unauthorized() {
-  const router = useRouter()
-  const [countdown, setCountdown] = useState(5)
-
-  useEffect(() => {
-    // Decrement the countdown every second
-    const interval = setInterval(() => {
-      setCountdown((prev) => prev - 1)
-    }, 1000)
-
-    // Redirect to the login page after 5 seconds
-    const timeout = setTimeout(() => {
-      router.push('/auth/login')
-    }, 5000)
-
-    // Clear timers on component unmount
-    return () => {
-      clearInterval(interval)
-      clearTimeout(timeout)
-    }
-  }, [router])
-
-  return (
-    <div>
-      <h2>Unauthorized</h2>
-      <p>Please log in to access this page.</p>
-      <p>Redirecting to the login page in {countdown} seconds...</p>
-      <p>OR</p>
-      <Link style={{ color: 'blue' }} href="/auth/login">
-        Login here
-      </Link>
-    </div>
-  )
+  redirect('/auth/login')
 }
